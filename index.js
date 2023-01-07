cc6a8f24d15f5eee9d425d604d0776b5eea8f6a4
Reese Marshall <reese.amarshall@gmail.com>
2023-01-06 23:28:27 -0500

    commit tree 8a03b674d08fd097ba58822eeb09bc64c3bc47af

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, event) {
+  const newCards =[]
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]},for the wonderful suprise gift!`);
+
+  }
+
+  return newCards;
+}
+
+writeCards(cards);
+
+
+
+
